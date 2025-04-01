7cc44c5d5609568d717ad57268d236e87999bbfa
hyeok <gjwnsgur0923@naver.com>
2025-04-01 14:41:57 +0900

    Update 시간표 분석(판다스)-허준혁.py

--- 시간표 분석(판다스)-허준혁.py
+++ 시간표 분석(판다스)-허준혁.py
@@ -13,7 +13,7 @@ df = pd.DataFrame(list1, columns = col_names)
 df.to_csv('./timetable.csv', header = True, index = False,
           encoding='utf-8')
 
-df2 = pd.read_csv('./timetable.csv', sep=',')
+df2 = pd.read_csv('./timetable.csv', sep = ',')
 df2['교수'] = ['김예희', '오정현', '인세훈', '이새봄', '배유진', '이가원']
 df2
 
